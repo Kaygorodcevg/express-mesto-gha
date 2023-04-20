7e954649370e9e6d029574f3160c076a3f8adb34
Gleb <113468160+Kaygorodcevg@users.noreply.github.com>
2023-04-21 00:19:27 +0500

    likes 8

--- models/card.js
+++ models/card.js
@@ -16,12 +16,11 @@ const cardSchema = new mongoose.Schema({
     ref: 'user',
     required: true,
   },
-  likes: {
+  likes: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
     default: [],
-    required: true,
-  },
+  }],
   createdAt: {
     type: Date,
     default: Date.now,
