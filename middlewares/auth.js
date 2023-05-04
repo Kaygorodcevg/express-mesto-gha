1f82031f7c96d6a0cde095040b637e8eeae094e4
Gleb <113468160+Kaygorodcevg@users.noreply.github.com>
2023-05-04 23:11:06 +0500

    test cook

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -12,7 +12,7 @@ module.exports = (req, res, next) => {
 
   let payload;
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
     return new UnauthorizedError('Необходима авторизация');
   }
