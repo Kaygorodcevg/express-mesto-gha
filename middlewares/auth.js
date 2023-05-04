51781b97be1175583f699348f32b41b72c6e5d6d
Gleb <113468160+Kaygorodcevg@users.noreply.github.com>
2023-05-04 23:04:00 +0500

    cookies

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../utils/UnauthorizedError');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
 
@@ -10,7 +12,7 @@ module.exports = (req, res, next) => {
 
   let payload;
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     return new UnauthorizedError('Необходима авторизация');
   }
