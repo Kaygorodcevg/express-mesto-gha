53435efcb128871f62d6b440f0db3531004e2a75
Gleb <113468160+Kaygorodcevg@users.noreply.github.com>
2023-05-04 22:48:07 +0500

    test serv

--- routes/users.js
+++ routes/users.js
@@ -25,9 +25,6 @@ router.get('/me', celebrate({
 // router.post('/', createUser);
 
 router.patch('/me', celebrate({
-  query: Joi.object().keys({
-    _id: Joi.string().alphanum().length(24),
-  }),
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
