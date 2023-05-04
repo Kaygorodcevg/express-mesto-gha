cab83dbd0b3b13dc4fb983e4246ec27aa4064c42
Gleb <113468160+Kaygorodcevg@users.noreply.github.com>
2023-05-04 22:39:01 +0500

    test auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,21 +1,18 @@
 const jwt = require('jsonwebtoken');
+const UnauthorizedError = require('../utils/UnauthorizedError');
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
 
   if (!token) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    return new UnauthorizedError('Необходима авторизация');
   }
 
   let payload;
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    return new UnauthorizedError('Необходима авторизация');
   }
   req.user = payload;
   return next();
