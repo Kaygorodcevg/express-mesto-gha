b495312798425155c2e31b8481abffc091b9ecf5
Gleb <113468160+Kaygorodcevg@users.noreply.github.com>
2023-05-04 23:17:22 +0500

    test cook 2

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,20 +1,18 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../utils/UnauthorizedError');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
-
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
 
   if (!token) {
-    return new UnauthorizedError('Необходима авторизация');
+    return next(new UnauthorizedError('Требуется авторизация!'));
   }
 
   let payload;
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    return new UnauthorizedError('Необходима авторизация');
+    return next(new UnauthorizedError('Требуется авторизация!'));
   }
   req.user = payload;
   return next();
