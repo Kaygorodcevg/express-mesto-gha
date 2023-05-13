977937d3a1c74d5aa995ea5dbe778239749bfcc9
Gleb <113468160+Kaygorodcevg@users.noreply.github.com>
2023-05-13 12:09:14 +0500

    feat:logger

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const validationErrors = require('celebrate').errors;
 const mainRouter = require('./routes');
 const err = require('./middlewares/error');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -13,7 +14,9 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(cookieParser());
 app.use(express.json());
 
+app.use(requestLogger);
 app.use('/', mainRouter);
+app.use(errorLogger);
 app.use(validationErrors());
 app.use(err);
 
